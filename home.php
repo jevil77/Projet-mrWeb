2b54ded8c4e46bfabd585fe35018046085e1b5a2
jevil77 <jerome.blondeau@uha.fr>
2025-01-04 21:50:51 +0100

    first commit

---
+++ home.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+    <link rel="stylesheet" href="style.css">
+  
+    <link rel="stylesheet"  href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+    <script src="https://kit.fontawesome.com/1e6654e711.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+
+<section class="header">
+
+      <a href="home.php" class="logo">travel.</a>
+        <nav class="navbar">
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="package.php">package</a>
+            <a href="book.php">book</a>
+
+        </nav>
+
+        <div id="menu-btn" class="fa-solid fa-bars"></div>
+            
+
+
+
+</section>
+
+
+
+
+<section class="home">
+    <div class="home-slider">
+
+    <div class="W">
+        <div class="slide" style="background:url(images/trees.jpg) no-repeat">
+            <div class="content">
+                <span>explore, discover, travel</span>
+                <h3>travel arround the world</h3>
+                <a href="package.php" class="btn">discover more</a>
+
+            </div>
+
+
+
+    
+        </div>
+
+
+        <div class="slide" style="background:url(images/trees.jpg) no-repeat">
+            <div class="content">
+                <span>explore, discover, travel</span>
+                <h3>discover the news places</h3>
+                <a href="package.php" class="btn">discover more</a>
+
+            </div>
+
+        </div>
+
+
+
+        <div class="slide" style="background:url(images/trees.jpg) no-repeat">
+            <div class="content">
+                <span>explore, discover, travel</span>
+                <h3>make your tour world</h3>
+                <a href="package.php" class="btn">discover more</a>
+
+            </div>
+
+
+
+
+
+
+
+
+
+
+    </div>
+
+
+
+    </div>
+
+</section>
+
+
+
+
+
+<section class="services">
+    <h1 class="heading-title"> our services</h1>
+    <box class="container">
+
+    <div class="box">
+        <img src="images/icon-1.png" alt="">
+        <h3>adventure</h3>
+
+
+
+    </div>
+    <div class="box">
+        <img src="
+" alt="">
+        <h3>tour guide</h3>
+
+
+
+    </div>
+
+    <div class="box">
+        <img src="" alt="">
+        <h3>trekking</h3>
+
+
+
+    </div><div class="box">
+        <img src="" alt="">
+        <h3>camp fire</h3>
+
+
+
+    </div><div class="box">
+        <img src="" alt="">
+        <h3>off road</h3>
+
+
+
+    </div>
+    <div class="box">
+        <img src="" alt="">
+        <h3>camping</h3>
+
+
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </box>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+                <a href="home.php"><i class="fas fa-angle-right"></i>home</a>
+                <a href="about.php"><i class="fas fa-angle-right"></i>about</a>
+                <a href="package.php"><i class="fas fa-angle-right"></i>package</a>
+                <a href="book.php"><i class="fas fa-angle-right"></i>book</a>
+
+
+           </div>
+
+
+
+           <div class="box">
+            <h3>extra links</h3>
+                <a href="#"><i class="fas fa-angle-right"></i>ask questions</a>
+                <a href="#"><i class="fas fa-angle-right"></i>about us</a>
+                <a href="#"><i class="fas fa-angle-right"></i>privacy politics</a>
+                <a href="#"><i class="fas fa-angle-right"></i>terms of use</a>
+              
+              
+
+
+           </div>
+
+
+           <div class="box">
+            <h3>contact info</h3>
+                <a href="#"><i class="fas fa-phone"></i> +06 11 22 33</a>
+                <a href="#"><i class="fas fa-phone"></i> +06 44 55 66</a>
+                <a href="#"><i class="fas fa-envelope"></i>jb@gmail.com</a>
+                <a href="#"><i class="fas fa-map"></i>123654789</a>
+              
+              
+
+
+           </div>
+
+
+
+           <div class="box">
+            <h3>follow us</h3>
+                <a href="#"><i class="fab fa-facebook"></i> facebook</a>
+                <a href="#"><i class="fab fa-twitter"></i> twitter / X</a>
+                <a href="#"><i class="fab fa-instagram"></i> instagram</a>
+                <a href="#"><i class="fas fa-map"></i>123654789</a>
+              
+              
+
+
+           </div>
+
+
+           <div class="credit">created by <span>jevil77</span></div>
+    
+    
+    
+    
+
+    </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+<script src="js/script.js"></script>    
+</body>
+</html>
---
+++ package.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about</title>
+    <link rel="stylesheet" href="style.css">
+  
+    <link rel="stylesheet"  href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+    <script src="https://kit.fontawesome.com/1e6654e711.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+
+<section class="header">
+
+      <a href="home.php" class="logo">travel.</a>
+        <nav class="navbar">
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="package.php">package</a>
+            <a href="book.php">book</a>
+
+        </nav>
+
+        <div id="menu-btn" class="fa-solid fa-bars"></div>
+            
+
+
+
+</section>
+
+
+<div class="heading img_about">
+    <h1>packages</h1>
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+                <a href="home.php"><i class="fas fa-angle-right"></i>home</a>
+                <a href="about.php"><i class="fas fa-angle-right"></i>about</a>
+                <a href="package.php"><i class="fas fa-angle-right"></i>package</a>
+                <a href="book.php"><i class="fas fa-angle-right"></i>book</a>
+
+
+           </div>
+
+
+
+           <div class="box">
+            <h3>extra links</h3>
+                <a href="#"><i class="fas fa-angle-right"></i>ask questions</a>
+                <a href="#"><i class="fas fa-angle-right"></i>about us</a>
+                <a href="#"><i class="fas fa-angle-right"></i>privacy politics</a>
+                <a href="#"><i class="fas fa-angle-right"></i>terms of use</a>
+              
+              
+
+
+           </div>
+
+
+           <div class="box">
+            <h3>contact info</h3>
+                <a href="#"><i class="fas fa-phone"></i> +06 11 22 33</a>
+                <a href="#"><i class="fas fa-phone"></i> +06 44 55 66</a>
+                <a href="#"><i class="fas fa-envelope"></i>jb@gmail.com</a>
+                <a href="#"><i class="fas fa-map"></i>123654789</a>
+              
+              
+
+
+           </div>
+
+
+
+           <div class="box">
+            <h3>follow us</h3>
+                <a href="#"><i class="fab fa-facebook"></i> facebook</a>
+                <a href="#"><i class="fab fa-twitter"></i> twitter / X</a>
+                <a href="#"><i class="fab fa-instagram"></i> instagram</a>
+                <a href="#"><i class="fas fa-map"></i>123654789</a>
+              
+              
+
+
+           </div>
+
+
+           <div class="credit">created by <span>jevil77</span></div>
+    
+    
+    
+    
+
+    </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+<script src="js/script.js"></script>    
+</body>
+</html>
